62e19dab2acb469844953e2a4ceb603bae802431
Sabajmukhadze1996 <sabajmukhadze23@gmail.com>
2022-07-02 05:26:18 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Footer } from './Footer';
 import { Projects } from './Projects';
 import myImage from './saba.jpg'
 import { SkillsCard } from './SkillsCard';
+import earthImg from "./earth.png";
 
 const downArrow = <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
@@ -18,6 +19,7 @@ const toolIcon = <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" 
 <path d="M12.496 8a4.491 4.491 0 0 1-1.703 3.526L9.497 8.5l2.959-1.11c.027.2.04.403.04.61Z"/>
 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1 0a7 7 0 1 0-13.202 3.249l1.988-1.657a4.5 4.5 0 0 1 7.537-4.623L7.497 6.5l1 2.5 1.333 3.11c-.56.251-1.18.39-1.833.39a4.49 4.49 0 0 1-1.592-.29L4.747 14.2A7 7 0 0 0 15 8Zm-8.295.139a.25.25 0 0 0-.288-.376l-1.5.5.159.474.808-.27-.595.894a.25.25 0 0 0 .287.376l.808-.27-.595.894a.25.25 0 0 0 .287.376l1.5-.5-.159-.474-.808.27.596-.894a.25.25 0 0 0-.288-.376l-.808.27.596-.894Z"/>
 </svg>
+
 
 
 function App() {
